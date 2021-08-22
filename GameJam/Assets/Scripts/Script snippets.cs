8c43c68d8b6c2878a33c4387cdd3438997298c06
Stenosa <bn0011ad@gmail.com>
2021-08-22 19:41:04 +0800

    Script Snippets gained a timer
    Timer template put into script snippets.

--- GameJam/Assets/Scripts/Script snippets.cs
+++ GameJam/Assets/Scripts/Script snippets.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
 public class Scriptsnippets : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -14,5 +15,20 @@ public class Scriptsnippets : MonoBehaviour
     void Update()
     {
         
+    }
+}
+
+
+TIMER!
+
+timer = value;
+
+void Update()
+{
+timer -= Time.deltaTime;
+if(timer <= 0)
+    {
+    timer = value;
+    //effect script here, for example: spawn object.
     }
 }
