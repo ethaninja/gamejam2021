15ab993da6ec68e34e7609d428b6eb1fe10a7d63
Me Me Me <ethaninja@hotmail.com>
2021-08-24 19:09:36 +1000

    Added Enemy Follow Script
    It's a bit janky currently but the enemy will now follow the player around.
    BE ADVISED: As I configured a lot of this in my scene and didn't make it some damn prefabs it won't work until you link up shit properly. So I will do so now and it shouldn't be a problem.
    
    Also, we need to move the movement/input/etc off the player_model and onto the base of the player :)  The model should only be the model and the parent Player object should drive everything

---
+++ GameJam/Assets/PlayerInfo.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Ignore the name PlayerInfo, it was a shit name I just made to have something to start with
+public class PlayerInfo : MonoBehaviour
+{
+    //I think in here we will have variables specific to the player. We can reference the PlayerInput script
+    //in order to modify(override) default values such as speed when the player picks up shit
+
+    public GameObject playerPos;
+    public float reactTimer = 0.5f; //How often the target pos in the enemy script will check for your new position
+
+    private void Update() 
+    {
+        if(reactTimer > 0)
+        {
+            reactTimer -= Time.deltaTime;
+        }    
+        if(reactTimer <= 0)
+        {
+            UpdatePosition();
+            reactTimer = 0.5f;
+        }
+    }
+
+    void UpdatePosition()
+    {
+        playerPos.transform.position = transform.position; //Now our playerPos will change to the players actual location
+        //each time it updates.
+    }
+}
--- GameJam/Assets/Scripts/Enemy.cs
+++ GameJam/Assets/Scripts/Enemy.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    float health;
-    public bool inRange;
+    
+    public float health;
+    public float attackDamage;
+    public float rateOfFire;
+    public bool canFire;
+    float fireTime;
 
+    public GameObject target;
+    public Vector3 targetPos;
+    public bool inRange;
     public Animator animatorRef;
+    public float moveSpeed;
+    //public float turnSpeed; 
 
     // Start is called before the first frame update
     void Start()
@@ -16,31 +25,50 @@ public class Enemy : MonoBehaviour
     }
     void Awake() 
     {
-        animatorRef.GetComponent<Animator>();    
+        animatorRef.GetComponent<Animator>();
+        target = GameObject.Find("Model_Player"); //Although we can drag this into inspector, it's good
+        //practice to use code to assign things. Be carefull about how you do it though
+        //as some ways are more resource heavier than others
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Here we continue to update the target (players) position for the enemy so they know where to move 
+        //when the player moves
+        targetPos = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+        //Now we actively move the transform.position of the enemy object using Unity's MoveTowards function
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
     }
+
+
 
     void OnTriggerEnter(Collider other) 
     {
         if(other.tag == "Player")
         {
             inRange = true;
-            Debug.Log(inRange);
-            animatorRef.SetTrigger("inRange");
+            if(fireTime >= rateOfFire)
+            {
+                Attack();
+            }
+            
         }    
     }
     void OnTriggerExit(Collider other) 
     {
         if(other.tag == "Player")
         {
+            fireTime = 0; //Resets the fire timer when the player leaves range
             inRange = false;
-            Debug.Log(inRange);
             animatorRef.SetTrigger("inRange");
         }
+    }
+
+
+    void Attack()
+    {
+        animatorRef.SetTrigger("inRange");
+        Debug.Log("rawr!");
     }
 }
