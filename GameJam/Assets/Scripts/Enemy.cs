2149f2db126ed081a333653eea49134e6c7f4f46
Me Me Me <ethaninja@hotmail.com>
2021-08-24 20:10:32 +1000

    Enemy now rotates towards player
    Added some code to rotate enemy towards players known location. A bit choppy but it works

--- GameJam/Assets/Scripts/Enemy.cs
+++ GameJam/Assets/Scripts/Enemy.cs
@@ -11,12 +11,14 @@ public class Enemy : MonoBehaviour
     public bool canFire;
     float fireTime;
 
+
     public GameObject target;
+    public Transform rotationToTarget;
     public Vector3 targetPos;
-    public bool inRange;
     public Animator animatorRef;
+    public bool inRange;
     public float moveSpeed;
-    //public float turnSpeed; 
+    public float turnSpeed; 
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +31,7 @@ public class Enemy : MonoBehaviour
         target = GameObject.Find("Model_Player"); //Although we can drag this into inspector, it's good
         //practice to use code to assign things. Be carefull about how you do it though
         //as some ways are more resource heavier than others
+        //ALSO, we need to get all the Input and Colliders/Rigidbody into the Player Parent Object
     }
 
     // Update is called once per frame
@@ -37,8 +40,15 @@ public class Enemy : MonoBehaviour
         //Here we continue to update the target (players) position for the enemy so they know where to move 
         //when the player moves
         targetPos = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+
         //Now we actively move the transform.position of the enemy object using Unity's MoveTowards function
         transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+
+        //Rotate towards player
+        Vector3 targetDirection = rotationToTarget.position - transform.position;
+        float singleStep = turnSpeed * Time.deltaTime;
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+        transform.rotation = Quaternion.LookRotation(newDirection);
     }
 
 
