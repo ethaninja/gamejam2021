56cf0860bcb2f12f60663a50adba27a0e8fb073c
Me Me Me <ethaninja@hotmail.com>
2021-08-25 20:42:05 +1000

    Added Camera Follow script
    You can now just drag the Camera Prefab onto your scene anywhere and it should work.
    If you want to adjust the position of the camera, just use the Variables in the Camera Follow script instead of physically moving the camera with its Transform component.
    BE SURE TO REFERENCE THE PLAYER in the Target variable in the Inspector.

---
+++ GameJam/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target; 
+    private GameObject playerRef;
+
+    public float smoothSpeed = 0.2f; //How fast the camera will follow the target
+    public Vector3 offset; //offset camera on all 3 axis
+
+    void Awake() 
+    {
+            //target = transform.Find("Player"); //Isn't working. For now you'll have to manually reference it in inspector
+    }
+    //LateUpdate runs right after the Update
+    void LateUpdate() 
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+
+}
