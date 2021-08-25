dba880a8176838860f3a3576ae6e7a944764ce00
Stenosa <bn0011ad@gmail.com>
2021-08-25 22:45:30 +0800

    Updating prefabs
    as above

--- GameJam/Assets/Scripts/Billboard.cs
+++ GameJam/Assets/Scripts/Billboard.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
-    public Transform cam;
+    //public Transform cam;
+    public GameObject cam;
 
+    void Awake()
+    {
+        cam = this.GetComponent<GameObject>();
+        
+    }
+    
     void LateUpdate()
     {
-        transform.LookAt(transform.position + cam.forward);
+        transform.LookAt(transform.position + cam.transform.forward);
 
     }
 }
